07feb3925b9891317448488acde22f2745c12995
bosiecki <bnosiecki@wisc.edu>
2023-05-26 21:16:54 -0500

    Added Traversium Block, Ore, and Deepslate Ore

--- src/main/java/net/youniversehq/funtravel/Block/ModBlocks.java
+++ src/main/java/net/youniversehq/funtravel/Block/ModBlocks.java
@@ -1,14 +1,19 @@
 package net.youniversehq.funtravel.Block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.youniversehq.funtravel.FunTravel;
+import net.youniversehq.funtravel.item.ModCreativeModeTab;
 import net.youniversehq.funtravel.item.ModItems;
 
 import java.util.function.Supplier;
@@ -18,6 +23,20 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, FunTravel.MOD_ID);
 
+    public static final RegistryObject<Block> TRAVERSIUM_BLOCK = registerBlock("traversium_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).explosionResistance(100).requiresCorrectToolForDrops()),
+                    ModCreativeModeTab.FUNTRAVEL_TAB);
+
+    public static final RegistryObject<Block> TRAVERSIUM_ORE = registerBlock("traversium_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).explosionResistance(100).requiresCorrectToolForDrops(), UniformInt.of(3, 5)),
+            ModCreativeModeTab.FUNTRAVEL_TAB);
+
+    public static final RegistryObject<Block> DEEPSLATE_TRAVERSIUM_ORE = registerBlock("deepslate_traversium_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).explosionResistance(100).requiresCorrectToolForDrops(), UniformInt.of(3, 5)),
+            ModCreativeModeTab.FUNTRAVEL_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
